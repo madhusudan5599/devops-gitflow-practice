3b1ed3bba18fe8c2c6ba4a3aea76114506683270
Madhusudan <madhusudan5599@gmail.com>
2025-04-06 09:29:02 +0530

    remove all unnecessary files

--- remote-local-branch-syn.sh
+++
@@ -1,7 +0,0 @@
-for remote_branch in $(git branch -r | grep -v 'HEAD'); do
-  local_branch=${remote_branch#origin/}
-  if ! git show-ref --quiet refs/heads/$local_branch; then
-    git branch --track $local_branch $remote_branch
-  fi
-done
-
